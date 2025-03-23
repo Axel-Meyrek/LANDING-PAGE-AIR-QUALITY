fd5f317a6ea0ebd6146e00021d665f03fde7f71e
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-03-23 12:41:19 -0600

    Add function CalRisk and show in the card

--- js/index.js
+++ js/index.js
@@ -1,14 +1,39 @@
 /* VARIABLES Y COMPONENTES */
-/* const $riesgo = document.querySelector('#riesgo'); */
+const $riesgo = document.querySelector('#riesgo');
 
 /* FUNCIONES */
 const calcularRiesgo = async () => {
+
+    /* CONFIGURACCION API */
+    const URL = 'https://iaq-fast-api.vercel.app/Risk';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {
+            "Temperature": 25.0,
+            "RelativeHumidity": 40.0,
+            "DoorOpen": true,
+            "NaturalVentilation": true,
+            "NormalVentilation": false,
+            "PeopleDensity": 0.5,
+            "CO2": 500,
+            "TVOC": 80,
+            "PM25": 24,
+            "PM10": 24,
+            "Model": "model_risk"
+          })
+    };
+
+    /* PETICIÓN */
     try {
-        const respuesta = await fetch("https://iaq-fast-api.vercel.app");
+        const respuesta = await fetch(URL, options);
         const data = await respuesta.json();
         console.log(data);
+        $riesgo.textContent = data.Value;
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 }
 
